bd926f1b55d773d4b2a106965dcbc1539bc74fb8
crono628 <84817352+crono628@users.noreply.github.com>
2024-07-16 20:27:28 -0400

    chore: Update CSS for improved responsiveness and layout

--- src/components/CollapsibleSchedule/CollapsibleSchedule.jsx
+++ src/components/CollapsibleSchedule/CollapsibleSchedule.jsx
@@ -3,6 +3,7 @@ import { useAppContext } from '../AppContext/AppContext'
 import { logEvent } from '@firebase/analytics'
 import { analytics } from '../../firebase'
 import './CollapsibleSchedule.css'
+import Fade from '@mui/material/Fade'
 
 const CollapsibleSchedule = ({ obj }) => {
   if (!obj) return null
@@ -58,7 +59,11 @@ const CollapsibleSchedule = ({ obj }) => {
   }, [isOpenAll])
 
   return (
-    <div onMouseOver={handleMouseOver} onMouseLeave={() => setMouseOver(false)}>
+    <div
+      className="collapsible-wrapper"
+      onMouseOver={handleMouseOver}
+      onMouseLeave={() => setMouseOver(false)}
+    >
       <div className="collapsible" onClick={toggleOpen}>
         <div>{provider} </div>
         <span
@@ -73,22 +78,23 @@ const CollapsibleSchedule = ({ obj }) => {
           ({today.length})
         </span>
       </div>
-      <div className="collapsible-content">
-        {isOpen && (
-          <>
-            {' ' + formatAppointmentTimes()}
-
-            {isOpen && mouseOver ? (
-              <div data-provider={provider} className="edit-delete-container">
-                <button onClick={handleEdit}>Edit</button>
-                <button onClick={handleDelete}>Delete</button>
-              </div>
-            ) : (
-              <div className="edit-delete-container"></div>
-            )}
-          </>
-        )}
-      </div>
+      <Fade in={isOpen} timeout={300}>
+        <div className="collapsible-content">
+          {isOpen && (
+            <>
+              {isOpen && mouseOver ? (
+                <div data-provider={provider} className="edit-delete-container">
+                  <button onClick={handleEdit}>Edit</button>
+                  <button onClick={handleDelete}>Delete</button>
+                </div>
+              ) : (
+                <div className="edit-delete-container"></div>
+              )}
+              {' ' + formatAppointmentTimes()}
+            </>
+          )}
+        </div>
+      </Fade>
     </div>
   )
 }
